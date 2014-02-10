1be1a123da15a4b5ad0ed2c6570f84f181d11354
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-02-10 12:05:09 -0500

    WhitePages API: Only reload page if the first load has the UUID for the cookie
    Change-Id: Ifad528f06f16a6a5b61bcad88921b8df49a08612

--- src/com/android/dialer/lookup/whitepages/WhitePagesApi.java
+++ src/com/android/dialer/lookup/whitepages/WhitePagesApi.java
@@ -56,18 +56,15 @@ public class WhitePagesApi {
         mOutput = httpGet(LOOKUP_URL + mNumber);
     }
 
-    private void extractCookie() throws IOException {
+    private void findUuidAndReload() throws IOException {
         for (String regex : COOKIE_REGEXES) {
             Pattern p = Pattern.compile(regex, Pattern.DOTALL);
             Matcher m = p.matcher(mOutput);
             if (m.find()) {
                 mCookie = m.group(1).trim();
+                fetchPage();
                 break;
             }
-        }
-
-        if (mCookie == null) {
-            throw new IOException("HTML response does not contain cookie value");
         }
     }
 
@@ -205,12 +202,8 @@ public class WhitePagesApi {
 
     public ContactInfo getContactInfo() throws IOException {
         if (mInfo == null) {
-            // We'll fetch the page twice every time since we do not what
-            // restrictions on the cookies are present (IP checks, time limits,
-            // etc.)
             fetchPage();
-            extractCookie();
-            fetchPage();
+            findUuidAndReload();
 
             buildContactInfo();
         }
